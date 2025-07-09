6187f1f71ad34dab91475f3f331b244a51f757c2
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-07-08 21:02:41 -0500

    feat(home): add home page

---
+++ src/app/home/pages/homepage/homepage.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css'
+})
+export class HomePageComponent {
+
+}
