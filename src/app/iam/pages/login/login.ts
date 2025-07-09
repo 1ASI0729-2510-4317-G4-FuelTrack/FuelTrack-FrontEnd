4864ebdd74a6c3ebcbd902b165618eb038670f61
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-07-08 21:02:04 -0500

    feat(login): add login page

---
+++ src/app/iam/pages/login/login.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent {
+  isClient = true;
+  email = '';
+  ruc = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.isClient) {
+      console.log(`[Client] Email:`, this.email);
+      this.router.navigate(['/orders']);
+    } else {
+      console.log(`[Supplier] RUC:`, this.ruc);
+      this.router.navigate(['/validations']);
+    }
+    console.log('Password:', this.password);
+  }
+
+  toggleLogin() {
+    this.isClient = !this.isClient;
+    this.email = '';
+    this.ruc = '';
+    this.password = '';
+  }
+}
