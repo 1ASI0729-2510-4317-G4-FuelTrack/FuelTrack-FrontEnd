06521ddc6805d18dcde73904970a01bc68264b09
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-07-08 21:05:23 -0500

    feat(orders): add order service for backend connection

--- src/app/orders/services/order.service.ts
+++ src/app/orders/services/order.service.ts
@@ -1,25 +1,25 @@
+// src/app/orders/services/order.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Order, OrderPay } from '../models/order.model';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class OrderService {
-  private baseUrl = `${environment.apiUrl}/orders`; // 👈 importante
+  private api = `${environment.apiUrl}/orders`;
 
   constructor(private http: HttpClient) {}
 
-  getAllOrders(): Observable<any> {
-    return this.http.get(this.baseUrl);
+  getAll(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.api);
   }
 
-  createOrder(orderData: any): Observable<any> {
-    return this.http.post(this.baseUrl, orderData);
+  createOrder(order: Partial<Order>): Observable<Order> {
+    return this.http.post<Order>(this.api, order);
   }
 
-  processOrder(orderId: string): Observable<any> {
-    return this.http.put(`${this.baseUrl}/${orderId}`, {});
+  registerPayment(orderId: string, payment: OrderPay): Observable<any> {
+    return this.http.post(`${this.api}/${orderId}/payments`, payment);
   }
 }
